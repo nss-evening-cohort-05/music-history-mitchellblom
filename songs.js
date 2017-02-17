ef15184ba9620ba5916eed50edc903899a591967
Mitchell Blom <mitchellblom@gmail.com>
2017-02-16 18:32:56 -0600

    step 1 complete

--- songs.js
+++ songs.js
@@ -5,3 +5,21 @@ songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast 
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+console.log(songs);
+
+// 1. Each student must add one song to the beginning and the end of the array.
+
+songs.unshift("Atlantic by Thrice on the album Vheissu");
+songs.push("Clipping by Mutemath on the album Armistice");
+console.log("with added songs: ", songs);
+
+// 2. Loop over the array and remove any words or characters that obviously don't belong.
+
+
+
+
+// 3. Students must find and replace the > character in each item with a - character.
+// 4. Must add each string to the DOM in index.html in the main content area.
+
+// map method - make array of the modified strings
