aa658807fdcb0461bf68c7a70966b2939b5b1c70
Mitchell Blom <mitchellblom@gmail.com>
2017-02-23 13:31:32 -0600

    hidden classes added, next look at links

--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,9 +1,9 @@
 var songs = [];
 
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
 console.log("original array: ", songs);
