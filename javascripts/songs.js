47cb34dd0a2f0521a314d13a63454b2f038709d9
Mitchell Blom <mitchellblom@gmail.com>
2017-02-25 00:25:48 -0600

    add fields writing to dom - need to match inputs

--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,4 +1,12 @@
+////////////////////// REFERENCES ////////////////////////////////////////////////////
+
+var addSongButton = document.getElementById("clickToAddSong");
+var addSongTitle = document.getElementById("addTitleField");
+var addSongArtist = document.getElementById("addArtistField");
+var addSongAlbum = document.getElementById("addAlbumField");
 var songs = [];
+
+////////////////////// STARTING ELEMENTS ///////////////////////////////////////////
 
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 // songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
@@ -6,20 +14,20 @@ songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 // songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-// console.log("original array: ", songs);
+////////////////////// ADDING SONGS FOR PART 2 ////////////////////////////////////
 
-// 1. Each student must add one song to the beginning and the end of the array.
+	// 1. Each student must add one song to the beginning and the end of the array.
 
 songs.unshift("Atlantic - by Thrice on the album Vheissu");
 songs.push("Clipping - by Mutemath on the album Armistice");
 
-// console.log("with added songs: ", songs);
+////////////////////// CLEAN SYMBOLS AND WRITE TO DOM ////////////////////////////
 
-// 2. Loop over the array and remove any words or characters that obviously don't belong.
-// 3. Students must find and replace the > character in each item with a - character.
-// 4. Must add each string to the DOM in index.html in the main content area.
+	// 2. Loop over the array and remove any words or characters that obviously don't belong.
+	// 3. Students must find and replace the > character in each item with a - character.
+	// 4. Must add each string to the DOM in index.html in the main content area.
 
-function removeSymbols(array) {
+function cleanAndWrite(array) {
 for (i = 0; i < array.length; i++) {
 	array[i] = array[i].replace(">", "-")
 	array[i] = array[i].replace("*", "");
@@ -30,7 +38,14 @@ for (i = 0; i < array.length; i++) {
 	array[i] = array[i].replace("on the album", "|");
 	document.getElementById("song" + i).innerHTML = array[i];
 	}
-	// console.log(array);
 };
 
-removeSymbols(songs);
+cleanAndWrite(songs);
+
+////////////////////// ADDING SONGS FROM PSEUDO-FORM ////////////////////////////
+
+addSongButton.addEventListener("click", function(event) {
+	songs.push(addSongTitle + " | " + addSongArtist + " | " + addSongAlbum);
+	console.log(songs);
+	cleanAndWrite(songs);
+});
