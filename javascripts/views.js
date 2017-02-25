441b925dc4a35f0e79e81c88b693144f70cd20da
Mitchell Blom <mitchellblom@gmail.com>
2017-02-24 23:07:22 -0600

    views switching successfully

--- javascripts/views.js
+++ javascripts/views.js
@@ -1,21 +1,23 @@
 /////////////////// VIEW TOGGLE ///////////////////////////
 
+var listLink = document.getElementById("link-list");
+var addLink = document.getElementById("link-add");
 
-var listLink = document.getElementById("list-view");
-var addLink = document.getElementById("add-view");
+var listDiv = document.getElementById("list-view");
+var addDiv = document.getElementById("add-view");
 
 listLink.addEventListener("click", function(event) {
-  event.preventDefault();
-  addLink.classList.add("hidden");
-  addLink.classList.remove("visible");
-  listLink.classList.add("visible");
-  listLink.classList.remove("hidden");
+  // event.preventDefault();
+  addDiv.classList.add("hidden");
+  addDiv.classList.remove("visible");
+  listDiv.classList.add("visible");
+  listDiv.classList.remove("hidden");
 });
 
 addLink.addEventListener("click", function(event) {
-  event.preventDefault();
-  listLink.classList.add("hidden");
-  listLink.classList.remove("visible");
-  addLink.classList.add("visible");
-  addLink.classList.remove("hidden");
+  // event.preventDefault();
+  listDiv.classList.add("hidden");
+  listDiv.classList.remove("visible");
+  addDiv.classList.add("visible");
+  addDiv.classList.remove("hidden");
 });
