fd0537fa79a13f6d10b20ef460deed04f5a96abc
Mitchell Blom <mitchellblom@gmail.com>
2017-04-03 20:02:39 -0500

    song getter working between iifes

--- javascripts/SongIife.js
+++ javascripts/SongIife.js
@@ -1,32 +1,32 @@
-var Songs = (function(oldSongs){
+var Songs = (function(oldSongs) {
 
-	var data;
+    var data;
 
-	function getData(data){
-		var jData = JSON.parse(this.responseText);
-		data = jData.songs;
-		for (var i=0;i<data.length; i++){
-			Songs.writeNewSongToArray(data[i].name);
-		}
-		return data;
-	}
+    function getData(data) {
+        var jData = JSON.parse(this.responseText);
+        data = jData.songs;
+        for (var i = 0; i < data.length; i++) {
+            Songs.testFunction(data[i]);
+        }
+        return data;
+    }
 
-	function fileFailed(){
-		alert("Request Timed Out");
-	}
+    function fileFailed() {
+        alert("Request Timed Out");
+    }
 
-	var songRequest = new XMLHttpRequest();
-	songRequest.addEventListener("load", getData);
-	songRequest.addEventListener("error", fileFailed);
-	songRequest.open("GET","songs1.json");
-	songRequest.send();
+    var songRequest = new XMLHttpRequest();
+    songRequest.addEventListener("load", getData);
+    songRequest.addEventListener("error", fileFailed);
+    songRequest.open("GET", "songs1.json");
+    songRequest.send();
 
-	  oldSongs.getXhr = function(){
+    oldSongs.getXhr = function() {
 
-	  	return data;
+        return data;
 
-	  }
+    }
 
-	return oldSongs;
+    return oldSongs;
 
 })(Songs || {});
--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,4 +1,4 @@
-////////////////////// REFERENCES ////////////////////////////////////////////////////
+var Songs = (function(oldSongs) {
 
 var addSongButton = document.getElementById("clickToAddSong");
 var addSongTitle = document.getElementById("addTitleField");
@@ -6,14 +6,15 @@ var addSongArtist = document.getElementById("addArtistField");
 var addSongAlbum = document.getElementById("addAlbumField");
 var songContainer = document.getElementById("right");
 
-////////////// IIFE ////////////////////////
-
-var Songs = (function(oldSongs) {
+	// console.log(Songs.getData(data));
 
     var privateArrayOfSongs = [];
 
-    oldSongs.getNewSongArray = function() {
+    oldSongs.testFunction = function(songsPassedFromSongIife) {
+    	console.log("testFunction works:", songsPassedFromSongIife);
+    }
 
+    oldSongs.getNewSongArray = function() {
         return privateArrayOfSongs;
     }
 
@@ -27,7 +28,7 @@ var Songs = (function(oldSongs) {
 
 ////////////////////// ADDING SONGS FROM PSEUDO-FORM ////////////////////////////
 
-addSongButton.addEventListener("click", function(event) {
-    songs.push(addSongTitle.value + " | " + addSongArtist.value + " | " + addSongAlbum.value);
-    writeArray(songs);
-});
+// addSongButton.addEventListener("click", function(event) {
+//     songs.push(addSongTitle.value + " | " + addSongArtist.value + " | " + addSongAlbum.value);
+//     writeArray(songs);
+// });
